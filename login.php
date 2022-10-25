0781ab747bd11d8b7a984aae582a26d263134814
Fran Segui <franciscojsegui@gmail.com>
2022-10-25 20:28:32 -0300

    inicio de sesion completo

--- login.php
+++ login.php
@@ -1,31 +1,6 @@
-<?php
-require 'database.php';
 
-session_start();
 
-if (isset($_SESSION['user_id'])) {
-  header('Location: /php-login');
-}
-
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $records = $connect()->prepare('SELECT * FROM users WHERE email = :email AND password = password');
-  $records->bindParam(':email', $_POST['email']);
-  $records->execute();
-  $results = $records->fetch(PDO::FETCH_ASSOC);
-
-  $message = '';
-
-  if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-    $_SESSION['user_id'] = $results['id'];
-    header("Location: /php-login");
-  } else {
-    $message = 'No se encontro el usuario';
-  }
-}
-
-?>
-
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -35,20 +10,68 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     <link rel="stylesheet" href=".//assets/sass/style.css">
 </head>
 <body id="bodyForms">
-<?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
+
 
     <h1>Inicia sesion ó</h1>
     <a href="registro.php">Registrate</a>
     <div id="formulario">
         <form action="login.php" method="POST" id="formularios">
             <p>Ingresa tu email</p>
-            <input type="email" name="mail" id="nombre" required>
+            <input type="email" name="email" id="nombre1" required>
             <p>Ingresa tu contraseña</p>
             <input type="password" name="password" id="nombre" required>
             <input type="submit" value="Entrar" id="btnEnviar">
         </form>
     </div>
 </body>
+</html> -->
+
+<?php
+
+  session_start();
+
+  if (isset($_SESSION['user_id'])) {
+    header('Location: /php-login');
+  }
+  require 'database.php';
+
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    $records->bindParam(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      $_SESSION['user_id'] = $results['id'];
+      header("Location: /php-login/");
+    } else {
+      $message = 'Las credenciales no cohinciden con nuestra base de datos :(';
+    }
+  }
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Rulo Caba - Inicio de sesion</title>
+    <link rel="stylesheet" href=".//assets/sass/style.css">
+  </head>
+  <body>
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Login</h1>
+    <span>or <a href="registro.php">SignUp</a></span>
+
+    <form action="login.php" method="POST">
+      <input name="email" type="text" placeholder="Enter your email">
+      <input name="password" type="password" placeholder="Enter your Password">
+      <input type="submit" value="Submit">
+    </form>
+  </body>
 </html>
--- registro.php
+++ registro.php
@@ -1,25 +1,56 @@
 <?php
- require 'database.php';
 
- $message = '';
+  require 'database.php';
 
- if (!empty($_POST['email']) && !empty($_POST['password'])) {
-   $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-   $stmt = $conn->prepare($sql);
-   $stmt->bindParam(':email', $_POST['email']);
-   $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-   $stmt->bindParam(':password', $password);
+  $message = '';
 
-   if ($stmt->execute()) {
-     $message = 'Usuario creado!!';
-   } else {
-     $message = 'Error al crear el usuario :(';
-   }
- }
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'Usuario Creado correctamente';
+    } else {
+      $message = 'No pudimos crear tu usuario. Ponte en contacto con nosotros si el problema persiste';
+    }
+  }
 ?>
-
-
 <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Rulo Caba - Registro</title>
+    <link rel="stylesheet" href=".//assets/sass/style.css">
+  </head>
+  <body id="bodyForms">
+
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Registrate</h1>
+    <span>o <a href="login.php">Inicia Sesion</a></span>
+    <div id="formulario">
+      <form action="registro.php" method="POST">
+        <p>Ingresa tu email</p>
+        <input name="email" type="text">
+        <p>Ingresa tu contraseña</p>
+        <input name="password" type="password">
+        <p>Confirma tu contraseña</p>
+        <input type="password" name="confirmarContraseña">
+        <input type="submit" value="Registrarse" id="btnEnviar">
+      </form>
+    </div>
+
+  </body>
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -39,16 +70,14 @@
             <p>Ingresa tu email</p>
             <input type="email" name="email" id="nombre"required>
             <p>Ingresa tu contraseña</p>
-            <input type="password" name="password" id="nombre" required>
+            <input type="password" name="password" id="nombre2" required>
             <p>Confirma tu contraseña</p>
-            <input type="password" name="confirmarContraseña" id="nombre" required>
+            <input type="password" name="confirmarContraseña" id="contraseña" required>
+            <a href="https://mpago.la/27oMQr4" id="continuar"><input  type="button" value="Colaborar" class="boton1"></a>
             <input type="submit" value="Registrarse" id="btnEnviar">
-            <a href="./login.php"><input type="button" value="Iniciar Sesion" id="btnEnviar"></a>
-            <div id="coincidirPass"></div>
+            <!-- <a href="./login.php"><input type="button" value="Iniciar Sesion" id="btnEnviar"></a> -->
+            <!-- <div id="coincidirPass"></div>
         </form>
-        <?php if(!empty($message)): ?>
-          <p class="newUser"> <?= $message ?></p>
-        <?php endif; ?>
     </div>
 </body>
 </html>
